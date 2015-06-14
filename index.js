887e15b2f6fc4194ce245bb260889788261f4676
MrKireko <mrkireko@gmail.com>
2015-06-14 12:21:40 +0200

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if (/\bMSIE 6/.test(navigator.userAgent) && !window.opera) {
     document.location = "outdated.html";
-}if ((navigator.userAgent.indexOf('iPad') != -1) || (navigator.userAgent.indexOf('Android') != -1 && navigator.userAgent.indexOf('Mobile') == -1)) { /*checking if user uses a non-desktop browser*/
+} else if ((navigator.userAgent.indexOf('iPad') != -1) || (navigator.userAgent.indexOf('Android') != -1 && navigator.userAgent.indexOf('Mobile') == -1)) { /*checking if user uses a non-desktop browser*/
     document.location = "mobile.html";
-}else if ((navigator.userAgent.indexOf('Mobile') != -1)) {
+} else if ((navigator.userAgent.indexOf('Mobile') != -1)) {
     document.location = "mobile.html";
 }
